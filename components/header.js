b8ae88a0493af581badd892c7c65b621a3208845
Andrew <Ahmcfadden@gmail.com>
2019-05-09 14:09:22 -0700

    update header.js

--- components/header.js
+++ components/header.js
@@ -11,7 +11,7 @@ const Header = (props) => (
         <div className="content">
             <div className="inner">
                 <h1>Andrew McFadden</h1>
-                <p>A fully responsive site template designed by <a href="https://html5up.net">HTML5 UP</a> and released<br />
+                <p>I am just a guy<a href="https://html5up.net">HTML5 UP</a> and released<br />
                 for free under the <a href="https://html5up.net/license">Creative Commons</a> license.</p>
             </div>
         </div>
