72ae5cfaccfa65d26f0303cbc0d0ad39718096a4
Andrew <Ahmcfadden@gmail.com>
2019-05-09 13:55:13 -0700

    update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,9 @@
 import Head from "next/head"
 import stylesheet from 'styles/main.scss'
 
-import Header from "/components/Header"
-import Main from "/components/Main"
-import Footer from "/components/Footer"
+import Header from "components/Header"
+import Main from "components/Main"
+import Footer from "components/Footer"
 
 class IndexPage extends React.Component {
     constructor(props) {
